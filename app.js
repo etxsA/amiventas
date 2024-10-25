1660d8f5ea5d0f1a4f9e02988029b6228dea2d63
etxsA <93368439+etxsA@users.noreply.github.com>
2024-10-25 17:11:46 -0600

    Implementation of Dashboard

---
+++ app.js
@@ -0,0 +1,34 @@
+import e from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import "dotenv/config"
+
+// 
+import dashboardRoutes from './routes/dashboard.routes.js';
+const app = e();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+app.use(e.static("public"));
+app.use(e.json());
+
+
+// Routes 
+app.use(dashboardRoutes);
+
+app.get('/404', (req, res) => {
+    res.sendFile('404.html', { root: "public" });
+});
+
+
+// 404 
+app.use((req, res) => {
+    res.redirect("/404");
+});
+
+
+app.listen(process.env.LISTEN_PORT || 3000, () =>{
+    console.log(`Running on port ${process.env.LISTEN_PORT || 3000}`);
+})
---
+++ routes/dashboard.routes.js
@@ -0,0 +1,12 @@
+import e from "express";
+
+const router = e.Router();
+
+router.get('/dashboard', (req, res) => {
+    res.sendFile('dashboard.html', { root: "public" });
+});
+
+router.get('/dashboard/inventory', (req, res) => {
+    res.sendFile('inventory.html', {root: "public"});
+});
+export default router; 
